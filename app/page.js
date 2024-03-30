730c47fc2669ac751e3fa703009582e1dc2435b0
Naman Jindal <namanjindal113@gmail.com>
2024-03-30 22:41:47 +0530

    done

--- app/page.js
+++ app/page.js
@@ -8,25 +8,62 @@ function Home() {
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
 
+  function startStopWatch(){
+    if(intervalRef.current){
+      clearInterval(intervalRef.current)
+    }
+    startTime.current = Date.now();
+    intervalRef.current = setInterval(()=>{
+      setCurrentTime(Date.now())
+    },10)
+  }
+
+  let timeToDisplay = ((currentTime-startTime.current)/1000).toFixed(3) 
+
+  function stopStopWatch(){
+     clearInterval(intervalRef.current);
+  }
+
+  function addLap(){
+    setLaps((prev)=> [...prev, timeToDisplay])
+  }
+  function resetStopWatch(){
+    if(intervalRef.current){
+      clearInterval(intervalRef.current)
+    }
+    startTime.current = 0;
+    intervalRef.current = 0;
+    setLaps([]);
+    setCurrentTime(0);
+  }
+
   return (
     <div id="main">
       <section>
-        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
+        <h1 className='seconds-elapsed'>{timeToDisplay }</h1>
         <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+          <button className="start-btn" onClick={startStopWatch}>START</button>
+          <button className="stop-btn" onClick={stopStopWatch}>STOP</button>
+          <button className="lap-btn" onClick={addLap}>LAP</button>
+          <button className="reset-btn" onClick={resetStopWatch}>RESET</button>
         </section>
       </section>
-      <section className='lap-section'>
-        <h2>Laps</h2>
-        <section className='laps'>
-          <p>lap</p>
-          <p>lap</p>
-          <p>lap</p>
+      {laps?.length ?
+        <section className='lap-section' >
+          <h2>Laps</h2>
+          <section className='laps'>
+            {
+              laps.map((lap)=>{
+                return (
+                  <div>{lap}</div>
+                )
+              })
+            }
+            
+          </section>
         </section>
-      </section>
+        : null
+      }
     </div>
   )
 }
